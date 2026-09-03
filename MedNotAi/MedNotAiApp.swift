ac87b94c815a6ef4ad36aef510a3e0d71a930544
kemal <kemal.z.a@icloud.com>
2026-09-03 22:23:33 +0300

    Initial Commit

---
+++ MedNotAi/MedNotAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedNotAiApp.swift
+//  MedNotAi
+//
+//  Created by Kemal Zeitulaev on 03.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct MedNotAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
